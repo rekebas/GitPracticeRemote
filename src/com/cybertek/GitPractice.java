421d0de6795c27623bc98790e1f200170b893164
rekebas <aekebas@hotmail.com>
2020-07-16 03:22:15 -0500

    updates

--- src/com/cybertek/GitPractice.java
+++ src/com/cybertek/GitPractice.java
@@ -5,7 +5,8 @@ public class GitPractice {
     public static void main(String[] args) {
 
 
-System.out.println("Hello World");
+      System.out.println("Hello World");
+      System.out.println("Cybertek School");
 
 
 
